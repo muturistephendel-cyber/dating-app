51aa0776ac01f83d5dade677911b49fb3f58041f
muturistephendel-cyber <muturistephendel@gmail.com>
2026-06-26 21:31:58 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,21 @@
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  background: #f0e9e4;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#root {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+  min-height: 100vh;
+}
